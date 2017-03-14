1d88443ee1bc459ca872ad6c09de232e95b1b84b
Paul Moy <pmoy13@gmail.com>
2017-03-13 22:45:05 -0400

    Added SQLite support
    Added simple SQLite support, which is unfinished. This is just the proof
    that we can add a database to the project and write to / read from the
    database at runtime. More work needs to be done to encapsulate the
    methods for accessing the database, as well as adding functionality to
    the database itself.

---
+++ RPG Platform/Assets/Scripts/SQLite/SQLiteTest.cs
@@ -0,0 +1,38 @@
+﻿using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using UnityEngine;
+
+public class SQLiteTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string conn = "URI=file:" + Application.dataPath + "/Databases/DD5eSQLiteDatabase.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT value,name, randomSequence " + "FROM PlaceSequence";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int value = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int rand = reader.GetInt32(2);
+
+            Debug.Log("value= " + value + "  name =" + name + "  random =" + rand);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
